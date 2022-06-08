4cfaae94e3171d2bab61c2932348682e29c4c6c9
ReitoSerizawa <s.reitiger@gmail.com>
2022-06-08 10:08:29 -0400

    commit tree cc28b7aa9379f292c23f6edf1e3e72484a35c2b7

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+
+    validate :not_this_title
+
+    def not_this_title
+        if title == "True Facts"
+            errors.add(:title, "Invalid title")
+        end
+    end
+
 end
